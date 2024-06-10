0f7648061e8b76e8abd83a29ad6d77b2dc4b27fd
tiyoigusty <tiyooigustyy@gmail.com>
2024-06-10 22:43:10 +0700

    add env and make vercel json

--- frontend/src/libs/api.ts
+++ frontend/src/libs/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   headers: { Authorization: `Bearer ${localStorage.token}` },
 });
