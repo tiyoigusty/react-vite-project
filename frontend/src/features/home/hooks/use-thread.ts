54129a4c00c07372c91a74744e3d035975921b8c
tiyoigusty <tiyooigustyy@gmail.com>
2024-06-20 15:29:33 +0700

    (frontend): add hooks

--- frontend/src/features/home/hooks/use-thread.ts
+++ frontend/src/features/home/hooks/use-thread.ts
@@ -6,44 +6,42 @@ import { ThreadEntity } from "../entities/thread";
 import { CreateThreadDTO } from "../types/thread";
 
 export const useThread = () => {
-    const { data: threads, refetch } = useQuery<ThreadEntity[]>({
-        queryKey: ["threads"],
-        queryFn: getThreads,
-      });
-    
-      const { register, handleSubmit } = useForm<CreateThreadDTO>({
-        mode: "onSubmit",
-        // resolver: zodResolver(createThreadSchema),
-      });
-    
-      async function getThreads() {
-        const response = await api.get("/threads", {
-          headers: {Authorization: `Bearer ${localStorage.token}`}
-        });
-        return response.data;
-      }
-    
-      const { mutateAsync } = useMutation<
-        ThreadEntity,
-        AxiosError,
-        CreateThreadDTO
-      >({
-        mutationFn: (newThread) => {
-          const formData = new FormData();
-          formData.append("content", newThread.content);
-          formData.append("image", newThread.image[0]);
-          return api.post("/threads", formData);
-        },
-      });
-    
-      const onSubmit: SubmitHandler<CreateThreadDTO> = async (data) => {
-        try {
-            await mutateAsync(data);
-            refetch()
-        } catch(error) {
-            console.log(error);
-        }
-      };
+  const { data: threads, refetch } = useQuery<ThreadEntity[]>({
+    queryKey: ["threads"],
+    queryFn: getThreads,
+  });
 
-    return { threads, register, handleSubmit, onSubmit }
-}
+  const { register, handleSubmit } = useForm<CreateThreadDTO>({
+    mode: "onSubmit",
+    // resolver: zodResolver(createThreadSchema),
+  });
+
+  async function getThreads() {
+    const response = await api.get("/threads");
+    return response.data;
+  }
+
+  const { mutateAsync } = useMutation<
+    ThreadEntity,
+    AxiosError,
+    CreateThreadDTO
+  >({
+    mutationFn: (newThread) => {
+      const formData = new FormData();
+      formData.append("content", newThread.content);
+      formData.append("image", newThread.image[0]);
+      return api.post("/threads", formData);
+    },
+  });
+
+  const onSubmit: SubmitHandler<CreateThreadDTO> = async (data) => {
+    try {
+      await mutateAsync(data);
+      refetch();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { threads, register, handleSubmit, onSubmit };
+};
