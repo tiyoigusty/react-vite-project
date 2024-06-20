d8e6510f1b968a4e7ca9e5e49bf10bf93065424b
tiyoigusty <tiyooigustyy@gmail.com>
2024-06-20 15:34:25 +0700

    (backend): add reply service

---
+++ backend/src/services/reply.ts
@@ -0,0 +1,98 @@
+import { PrismaClient } from "@prisma/client";
+import { CreateThreadDTO, UpdatedThreadDTO } from "../dto/thread-dto";
+import { createThreadSchema } from "../validators/thread";
+import { v2 as cloudinary } from "cloudinary";
+
+const prisma = new PrismaClient();
+
+async function find() {
+  try {
+    return await prisma.reply.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            fullName: true,
+            username: true,
+            photoProfile: true,
+          }
+        }
+      }
+    });
+  } catch (error) {
+    throw new String(error);
+  }
+}
+
+async function findOne(id: number) {
+    try {
+      const reply = await prisma.reply.findFirst({
+        where: { id },
+      });
+  
+      if (!reply) throw new String("THREAD NOT FOUND!!");
+      return reply;
+    } catch (error) {
+      throw new String(error);
+    }
+  }
+
+  async function create(dto: CreateThreadDTO, userId: number) {
+    try {
+      // validation with joi
+      const validate = createThreadSchema.validate(dto);
+      if (validate.error) {
+        throw new String(validate.error.message);
+      }
+  
+      cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+      });
+  
+      const upload = await cloudinary.uploader.upload(dto.image, {
+        upload_preset: "circle",
+      });
+  
+      return await prisma.reply.create({
+        data: { ...dto, userId, image: upload.secure_url },
+      });
+    } catch (error) {
+      throw new String(error);
+    }
+  }
+  
+  async function update(id: number, dto: UpdatedThreadDTO) {
+    try {
+      const reply = await prisma.reply.findFirst({
+        where: { id: Number(id) },
+      });
+  
+      if (dto.content) {
+        reply.content = dto.content;
+      }
+      if (dto.image) {
+        reply.image = dto.image;
+      }
+  
+      return await prisma.reply.update({
+        where: { id: Number(id) },
+        data: { ...reply },
+      });
+    } catch (error) {
+      throw new String(error);
+    }
+  }
+  
+  async function remove(id: number) {
+    try {
+      return await prisma.reply.delete({
+        where: { id: Number(id) },
+      });
+    } catch (error) {
+      throw new String(error);
+    }
+  }
+
+export default { find, findOne, create, update, remove };
