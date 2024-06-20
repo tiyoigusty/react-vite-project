1a7d7f4078fa6d7e9fdf4ce216add1b4efa2f2d7
tiyoigusty <tiyooigustyy@gmail.com>
2024-06-20 15:29:57 +0700

    (frontend): add toast

--- frontend/src/features/auth/hooks/use-register-form.ts
+++ frontend/src/features/auth/hooks/use-register-form.ts
@@ -2,10 +2,12 @@ import { useState } from "react";
 import { RegisterForm } from "../types/register-form";
 import { api } from "../../../libs/api";
 import { useNavigate } from "react-router-dom";
+import { useToast } from "@chakra-ui/react";
 
 export const useRegisterForm = () => {
   const navigate = useNavigate();
-
+  const toast = useToast();
+  
   const [register, setRegister] = useState<RegisterForm>({
     fullName: "",
     username: "",
@@ -26,9 +28,21 @@ export const useRegisterForm = () => {
   async function handleSubmit() {
     try {
       const response = await api.post("/auth/register", register);
+      toast({
+        title: "register success!",
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+      });
       navigate("/auth/login");
       console.log(response.data);
     } catch (error) {
+      toast({
+        title: "register failed!",
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+      });
       console.log(error);
     }
   }
