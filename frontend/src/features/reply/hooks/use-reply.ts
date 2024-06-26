95af4c495a5e87913bed71ac446d18ce41377b48
tiyoigusty <tiyooigustyy@gmail.com>
2024-06-26 21:01:37 +0700

    (frontend): edit hooks

--- frontend/src/features/reply/hooks/use-reply.ts
+++ frontend/src/features/reply/hooks/use-reply.ts
@@ -2,46 +2,46 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { api } from "../../../libs/api";
-import { ReplyEntity} from "../entities/reply";
-import { CreateReplyDTO,} from "../types/reply";
+import { ReplyEntity } from "../entities/reply";
+import { CreateReplyDTO } from "../types/reply";
+import { useParams } from "react-router-dom";
 
 export const useReply = () => {
-    const { data: replies, refetch } = useQuery<ReplyEntity[]>({
-        queryKey: ["replies"],
-        queryFn: getReplies,
-      });
-    
-      const { register, handleSubmit } = useForm<CreateReplyDTO>({
-        mode: "onSubmit",
-        // resolver: zodResolver(createThreadSchema),
-      });
-    
-      async function getReplies() {
-        const response = await api.get("/replies")
-        return response.data;
-      }
-    
-      const { mutateAsync } = useMutation<
-        ReplyEntity,
-        AxiosError,
-        CreateReplyDTO
-      >({
-        mutationFn: (newReplies) => {
-          const formData = new FormData();
-          formData.append("content", newReplies.content);
-          formData.append("image", newReplies.image[0]);
-          return api.post("/replies", formData);
-        },
-      });
-    
-      const onSubmit: SubmitHandler<CreateReplyDTO> = async (data) => {
-        try {
-            await mutateAsync(data);
-            refetch()
-        } catch(error) {
-            console.log(error);
-        }
-      };
+  const { data: replies, refetch } = useQuery<ReplyEntity[]>({
+    queryKey: ["replies"],
+    queryFn: getReplies,
+  });
 
-    return { replies, register, handleSubmit, onSubmit }
-}
+  const { register, handleSubmit } = useForm<CreateReplyDTO>({
+    mode: "onSubmit",
+    // resolver: zodResolver(createThreadSchema),
+  });
+
+  const {id} = useParams()
+
+  async function getReplies() {
+    const response = await api.get("/replies/"+id);
+    return response.data;
+  }
+
+
+  const { mutateAsync } = useMutation<ReplyEntity, AxiosError, CreateReplyDTO>({
+    mutationFn: (newReplies) => {
+      const formData = new FormData();
+      formData.append("content", newReplies.content);
+      formData.append("image", newReplies.image[0]);
+      return api.post("/replies/"+id, formData);
+    },
+  });
+
+  const onSubmit: SubmitHandler<CreateReplyDTO> = async (data) => {
+    try {
+      await mutateAsync(data);
+      refetch();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { replies, register, handleSubmit, onSubmit };
+};
---
+++ frontend/src/features/right-bar/hooks/use-profile.ts
@@ -0,0 +1,53 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { api } from "../../../libs/api";
+import { UserEntity } from "../../home/entities/user";
+import { CreateProfileDTO } from "../types/profile";
+import { useParams } from "react-router-dom";
+
+export const useProfile = () => {
+  const { data, refetch } = useQuery<UserEntity[]>({
+    queryKey: ["users"],
+    queryFn: getThreads,
+  });
+
+  const { register, handleSubmit } = useForm<CreateProfileDTO>({
+    mode: "onSubmit",
+    // resolver: zodResolver(createThreadSchema),
+  });
+
+  async function getThreads() {
+    const response = await api.get("/users");
+    return response.data;
+  }
+
+  const {id} = useParams()
+  
+  const { mutateAsync } = useMutation<
+    UserEntity,
+    AxiosError,
+    CreateProfileDTO
+  >({
+    mutationFn: (newProfile) => {
+      const formData = new FormData();
+      formData.append("background", newProfile.background[0]);
+      formData.append("photoProfile", newProfile.photoProfile[0]);
+      formData.append("fullName", newProfile.fullName);
+      formData.append("username", newProfile.username);
+      formData.append("bio", newProfile.bio);
+      return api.patch("/users/"+id, formData);
+    },
+  });
+
+  const onSubmit: SubmitHandler<CreateProfileDTO> = async (data) => {
+    try {
+      await mutateAsync(data);
+      refetch();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { data, register, handleSubmit, onSubmit };
+};
