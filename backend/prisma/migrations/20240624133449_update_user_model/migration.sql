14f38cec84fbb148f45ef778025c96115e7e0269
tiyoigusty <tiyooigustyy@gmail.com>
2024-06-26 21:04:57 +0700

    (backend): add new data at table user

---
+++ backend/prisma/migrations/20240624133449_update_user_model/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "background" TEXT NOT NULL DEFAULT 'https://images.pexels.com/photos/4439410/pexels-photo-4439410.jpeg?auto=compress&cs=tinysrgb&w=600';
